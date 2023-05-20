6a58d023c7cceb83120906f3fbe33965b8e1c66a
RoRo2023 <119353807+RoRo2023@users.noreply.github.com>
2023-05-19 21:29:57 -0500

    controllers y modals del catálogo de compras

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-const express = require('express'); // imports en node js sin ninguna configuracion
+const express = require('express'); 
 const app = express();
+
+//Usuarios
 const { getUsers, createUser, userDelete, userUpdate } = require('./controllers/users.controllers')
+//Catalogo
+const { getCatalog, createCatalog, catalogDelete, catalogUpdate } = require('./controllers/catalog.controllers')
+require('dotenv').config()  // importamos dotenv (conocidos como variables de entorno y se guardan en el archivo .env)
 const mongoose = require('mongoose');
-require('dotenv').config() // importamos dotenv (conocidos como variables de entorno y se guardan en el archivo .env)
+
 const port = process.env.PORT || 3000; // regularmente se usa el puerto 3000
 // || or, && and
  
@@ -10,6 +15,7 @@ const port = process.env.PORT || 3000; // regularmente se usa el puerto 3000
 app.use(express.json()) // nos permite que nuestra peticion post reciba informacion desde el body
  
 // Con Promise se puede trabajar con then y con catch
+
 mongoose.connect(process.env.HOSTDB).then(() => {
     console.log('Conexion a MongoDB');
 }).catch((error) => {
@@ -20,6 +26,11 @@ app.get('/user', getUsers);
 app.post('/user', createUser);
 app.put('/user/:id', userUpdate);
 app.delete('/user/:id', userDelete);
+
+app.get('/catalog', getCatalog);
+app.post('/catalog', createCatalog);
+app.put('/catalog/:id', catalogUpdate);
+app.delete('/catalog/:id', catalogDelete);
  
 // servidor
 app.listen(port, () => { // levanta el servidor
